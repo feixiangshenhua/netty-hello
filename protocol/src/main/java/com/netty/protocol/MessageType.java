7417a5dffc3a484b41895ecfe155dee94c690a23
xiaoyun <391720181@qq.com>
2016-04-10 20:51:32 +0800

    netty私有协议栈开发(编码解码)

---
+++ protocol/src/main/java/com/netty/protocol/MessageType.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2013-2018 Lilinfeng.
+ *  
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.netty.protocol;
+
+/**
+ * 常量配置
+ * Created by xiaoyun on 2016/4/10.
+ */
+public enum MessageType {
+    /**
+     * 业务类型：
+     * 0-业务请求 1-业务应答
+     * 2-半双工（既是请求也是响应）
+     * 3-握手请求 4-握手响应
+     * 5-心跳请求 6-心跳应答
+     */
+    SERVICE_REQ((byte) 0), SERVICE_RESP((byte) 1), ONE_WAY((byte) 2), LOGIN_REQ(
+            (byte) 3), LOGIN_RESP((byte) 4), HEARTBEAT_REQ((byte) 5), HEARTBEAT_RESP(
+            (byte) 6);
+
+    private byte value;
+
+    private MessageType(byte value) {
+        this.value = value;
+    }
+
+    public byte value() {
+        return this.value;
+    }
+}
