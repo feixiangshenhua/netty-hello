4e9262af1f7339a166b3633309bb2e0e61782dcc
xiaoyun <xiaoyun>
2016-04-10 19:30:35 +0800

    netty私有协议栈开发(编码解码)

---
+++ protocol/src/main/java/com/netty/protocol/codec/MarshallingDecoder.java
@@ -0,0 +1,34 @@
+package com.netty.protocol.codec;
+
+import io.netty.buffer.ByteBuf;
+import org.jboss.marshalling.ByteInput;
+import org.jboss.marshalling.Unmarshaller;
+
+import java.io.IOException;
+
+/**
+ * netty消息解码类
+ * Created by xiaoyun on 2016/4/10.
+ */
+public class MarshallingDecoder {
+    private final Unmarshaller unmarshaller;
+
+    public MarshallingDecoder() throws IOException {
+        this.unmarshaller = MarshallingCodeCFactory.buildUnMarshalling();
+    }
+
+    protected Object decode(ByteBuf in) throws Exception {
+        int objectSize = in.readInt();
+        ByteBuf buf = in.slice(in.readerIndex(), objectSize);
+        ByteInput input = new ChannelBufferByteInput(buf);
+        try {
+            unmarshaller.start(input);
+            Object obj = unmarshaller.readObject();
+            unmarshaller.finish();
+            in.readerIndex(in.readerIndex() + objectSize);
+            return obj;
+        } finally {
+            unmarshaller.close();
+        }
+    }
+}
