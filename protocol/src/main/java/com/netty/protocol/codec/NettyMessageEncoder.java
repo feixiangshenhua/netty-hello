e4354f058e08c8845b5e5ed8bf3035b7f18f24e3
xiaoyun <391720181@qq.com>
2016-04-11 23:52:44 +0800

    netty私有协议栈问题调试解决（编码解码问题）

--- protocol/src/main/java/com/netty/protocol/codec/NettyMessageEncoder.java
+++ protocol/src/main/java/com/netty/protocol/codec/NettyMessageEncoder.java
@@ -4,7 +4,7 @@ import com.netty.protocol.struct.NettyMessage;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.handler.codec.MessageToMessageEncoder;
+import io.netty.handler.codec.MessageToByteEncoder;
 
 import java.util.List;
 import java.util.Map;
@@ -13,7 +13,7 @@ import java.util.Map;
  * 消息编码类
  * Created by xiaoyun on 2016/4/10.
  */
-public final class NettyMessageEncoder extends MessageToMessageEncoder<NettyMessage> {
+public final class NettyMessageEncoder extends MessageToByteEncoder<NettyMessage> {
 
     MarshallingEncoder marshallingEncoder;
 
@@ -22,11 +22,9 @@ public final class NettyMessageEncoder extends MessageToMessageEncoder<NettyMess
     }
 
     @Override
-    protected void encode(ChannelHandlerContext ctx, NettyMessage msg, List<Object> out) throws Exception {
-        if(msg == null || msg.getHeader() == null) {
+    protected void encode(ChannelHandlerContext ctx, NettyMessage msg, ByteBuf sendBuf) throws Exception {
+        if (msg == null || msg.getHeader() == null)
             throw new Exception("The encode message is null");
-        }
-        ByteBuf sendBuf = Unpooled.buffer();
         sendBuf.writeInt((msg.getHeader().getCrcCode()));
         sendBuf.writeInt((msg.getHeader().getLength()));
         sendBuf.writeLong((msg.getHeader().getSessionID()));
@@ -36,7 +34,8 @@ public final class NettyMessageEncoder extends MessageToMessageEncoder<NettyMess
         String key = null;
         byte[] keyArray = null;
         Object value = null;
-        for (Map.Entry<String, Object> param : msg.getHeader().getAttachment().entrySet()) {
+        for (Map.Entry<String, Object> param : msg.getHeader().getAttachment()
+                .entrySet()) {
             key = param.getKey();
             keyArray = key.getBytes("UTF-8");
             sendBuf.writeInt(keyArray.length);
@@ -45,13 +44,12 @@ public final class NettyMessageEncoder extends MessageToMessageEncoder<NettyMess
             marshallingEncoder.encode(value, sendBuf);
         }
         key = null;
-        key = null;
         keyArray = null;
         value = null;
         if (msg.getBody() != null) {
             marshallingEncoder.encode(msg.getBody(), sendBuf);
         } else
             sendBuf.writeInt(0);
-        sendBuf.setInt(4, sendBuf.readableBytes());
+        sendBuf.setInt(4, sendBuf.readableBytes() - 8);
     }
 }
